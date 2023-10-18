af749d2c07c139277f212b0dc6f3bddf153fc749
souocare <gonga1999@outlook.com>
2023-10-18 20:34:50 +0100

    added solution to 2

--- examples/Projects/Classes/class20231012_mrjob/main.py
+++ examples/Projects/Classes/class20231012_mrjob/main.py
@@ -1,22 +1,37 @@
 import re
 import json
-
 from mrjob.job import MRJob
 from mrjob.step import MRStep
+from mrjob.compat import get_jobconf_value
+import os
 
 class WordCounter(MRJob):
 
+    def configure_args(self):
+        super(WordCounter, self).configure_args()
+        self.add_file_arg('--input', help='Input data path')
+        self.add_file_arg('--output', help='Output data path')
+        self.add_file_arg('--num-reducers', help='Number of reducers')
+        self.add_file_arg('--compression-codec', help='Compression codec (e.g., gzip)')
+
+    def load_args(self, args):
+        super(WordCounter, self).load_args(args)
+
+        # You can access the argument values using self.options
+        self.input_path = self.options.input
+        self.output_path = self.options.output
+        self.num_reducers = int(self.options.num_reducers)
+        self.compression_codec = self.options.compression_codec
+
     def mapper(self, key, value):
-        #value = value.decode('utf-8')  # Decode bytes to utf-8 string
-        
         for line in value:
             line = line.strip()
             tokens = re.findall(r"\b\w+\b", line)
             for token in tokens:
                 yield token, 1
 
-    def reducer(self, key, value):
-        yield None, (sum(value), key)
+    def reducer(self, key, values):
+        yield None, (sum(values), key)
 
     def reducer_sort(self, key, values):
         for count, key in sorted(values):
@@ -28,14 +43,21 @@ class WordCounter(MRJob):
     def steps(self):
         return [
             MRStep(
-                mapper=self.mapper, 
-                combiner = self.combiner,
-                reducer=self.reducer
+                mapper=self.mapper,
+                combiner=self.combiner,
+                reducer=self.reducer,
+                jobconf={
+                    'mapreduce.job.reduces': self.num_reducers  # Set the number of reducers
+                }
             ),
             MRStep(
-                reducer=self.reducer_sort
+                reducer=self.reducer_sort,
+                jobconf={
+                    'mapreduce.output.fileoutputformat.compress': 'true',
+                    'mapreduce.output.fileoutputformat.compress.codec': self.compression_codec  # Set compression codec
+                }
             )
         ]
-    
+
 if __name__ == '__main__':
     WordCounter.run()
