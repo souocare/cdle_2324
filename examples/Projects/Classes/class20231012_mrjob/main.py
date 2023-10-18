e7aea845316fe791fda286f983b09cbaaac9b4ba
souocare <gonga1999@outlook.com>
2023-10-18 23:33:07 +0100

    py script working in hadoop

--- examples/Projects/Classes/class20231012_mrjob/main.py
+++ examples/Projects/Classes/class20231012_mrjob/main.py
@@ -1,63 +1,20 @@
-import re
-import json
-from mrjob.job import MRJob
-from mrjob.step import MRStep
-from mrjob.compat import get_jobconf_value
 import os
+import mapreduce
+import sys
 
-class WordCounter(MRJob):
+list_of_arguments = sys.argv
+print(list_of_arguments)
 
-    def configure_args(self):
-        super(WordCounter, self).configure_args()
-        self.add_file_arg('--input', help='Input data path')
-        self.add_file_arg('--output', help='Output data path')
-        self.add_file_arg('--num-reducers', help='Number of reducers')
-        self.add_file_arg('--compression-codec', help='Compression codec (e.g., gzip)')
+input = list_of_arguments[1]
+output = list_of_arguments[2]
 
-    def load_args(self, args):
-        super(WordCounter, self).load_args(args)
+# added file to hadoop
+os.system("hadoop fs -put -f /home/usermr/examples/input/gutenberg-small/" + input + " /user/usermr/examples/input/gutenberg-small/")
 
-        # You can access the argument values using self.options
-        self.input_path = self.options.input
-        self.output_path = self.options.output
-        self.num_reducers = int(self.options.num_reducers)
-        self.compression_codec = self.options.compression_codec
+print('python3 mapreduce.py -r hadoop ' + input)
 
-    def mapper(self, key, value):
-        for line in value:
-            line = line.strip()
-            tokens = re.findall(r"\b\w+\b", line)
-            for token in tokens:
-                yield token, 1
+os.system('python3 mapreduce.py -r hadoop hdfs:///user/usermr/examples/input/gutenberg-small/' + input + " -output=" + output)
 
-    def reducer(self, key, values):
-        yield None, (sum(values), key)
+os.system("hadoop fs -copyToLocal /user/usermr/utput=/home/usermr/examples/output/gutenberg-small/ /home/usermr/examples/output/gutenberg-small/")
 
-    def reducer_sort(self, key, values):
-        for count, key in sorted(values):
-            yield count, key
 
-    def combiner(self, key, values):
-        yield key, sum(values)
-
-    def steps(self):
-        return [
-            MRStep(
-                mapper=self.mapper,
-                combiner=self.combiner,
-                reducer=self.reducer,
-                jobconf={
-                    'mapreduce.job.reduces': self.num_reducers  # Set the number of reducers
-                }
-            ),
-            MRStep(
-                reducer=self.reducer_sort,
-                jobconf={
-                    'mapreduce.output.fileoutputformat.compress': 'true',
-                    'mapreduce.output.fileoutputformat.compress.codec': self.compression_codec  # Set compression codec
-                }
-            )
-        ]
-
-if __name__ == '__main__':
-    WordCounter.run()
