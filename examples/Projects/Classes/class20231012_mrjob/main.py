a0fc7b81ee6d5a7e0b8811deb9da82674bd35b6d
souocare <gonga1999@outlook.com>
2023-10-18 23:39:52 +0100

    py script working in hadoop

--- examples/Projects/Classes/class20231012_mrjob/main.py
+++ examples/Projects/Classes/class20231012_mrjob/main.py
@@ -5,16 +5,23 @@ import sys
 list_of_arguments = sys.argv
 print(list_of_arguments)
 
-input = list_of_arguments[1]
-output = list_of_arguments[2]
 
-# added file to hadoop
-os.system("hadoop fs -put -f /home/usermr/examples/input/gutenberg-small/" + input + " /user/usermr/examples/input/gutenberg-small/")
+localorhadoop = list_of_arguments[1]
+input = list_of_arguments[2]
+output = list_of_arguments[3]
 
-print('python3 mapreduce.py -r hadoop ' + input)
+localorhadoop = "hadoop"
 
-os.system('python3 mapreduce.py -r hadoop hdfs:///user/usermr/examples/input/gutenberg-small/' + input + " -output=" + output)
+if localorhadoop == "hadoop":
 
-os.system("hadoop fs -copyToLocal /user/usermr/utput=/home/usermr/examples/output/gutenberg-small/ /home/usermr/examples/output/gutenberg-small/")
+    # added file to hadoop
+    os.system("hadoop fs -put -f /home/usermr/examples/input/gutenberg-small/" + input + " /user/usermr/examples/input/gutenberg-small/")
 
+    print('python3 mapreduce.py -r hadoop ' + input)
 
+    os.system('python3 mapreduce.py -r hadoop hdfs:///user/usermr/examples/input/gutenberg-small/' + input + " -output=" + output)
+
+    os.system("hadoop fs -copyToLocal /user/usermr/utput=/home/usermr/examples/output/gutenberg-small/ /home/usermr/examples/output/gutenberg-small/")
+
+else:
+    pass
