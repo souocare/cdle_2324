59c5bc9359019e2e95d515d983e6d570c3224115
souocare <gonga1999@outlook.com>
2023-11-08 22:42:22 +0000

    tried multiple ways to multiple laptops

--- examples/Projects/Classes/class20231012_mrjob/main.py
+++ examples/Projects/Classes/class20231012_mrjob/main.py
@@ -14,28 +14,39 @@ localorhadoop = "hadoop"
 
 if localorhadoop == "hadoop":
     # Remove files so it can generate the new ouputs
+    
     os.system("hadoop fs -rm -r /user/usermr/output")
+    os.system("hadoop fs -rm -r /home/usermr/examples/output")
     os.system("rm -r /home/usermr/examples/output/gutenberg-small/*")
 
-    destination_directory = "/user/usermr/examples/input/gutenberg-small/" + input
+    # remove input folder hadoop
+    destination_directory = "/user/usermr/examples/input/gutenberg-small/*" + input
     check_dir_command = "hadoop fs -test -e " + destination_directory
     directory_exists = os.system(check_dir_command) == 0
-  
-
     if not directory_exists:
 
         print(f"Destination directory {destination_directory} does not exist in HDFS. Creating it...")
         os.system("hadoop fs -mkdir -p " + destination_directory)
 
+
+    #remove output folder of user/usermr
     destination_directory_out = "/user/usermr/output/"
     check_dir_command_out = "hadoop fs -test -e " + destination_directory_out
     directory_exists_out = os.system(check_dir_command_out) == 0
-  
-
     if not directory_exists_out:
 
         print(f"Destination directory {destination_directory_out} does not exist in HDFS. Creating it...")
         os.system("hadoop fs -mkdir -p " + destination_directory_out)
+
+
+    #remove output folder of user/usermr
+    destination_directory_outhome = "/home/usermr/output/"
+    check_dir_command_outhome = "hadoop fs -test -e " + destination_directory_outhome
+    directory_exists_outhome = os.system(check_dir_command_outhome) == 0
+    if not directory_exists_outhome:
+
+        print(f"Destination directory {destination_directory_outhome} does not exist in HDFS. Creating it...")
+        os.system("hadoop fs -mkdir -p " + destination_directory_outhome)
 
 
 
@@ -47,7 +58,10 @@ if localorhadoop == "hadoop":
     os.system('python3 mapreduce.py -r hadoop hdfs:///user/usermr/examples/input/gutenberg-small/' + input + ' -o ' + output + ' -nr 2 -cc GzipCodec')
 
     print("")
-    os.system("hadoop fs -copyToLocal /user/usermr/output/ /home/usermr/examples/")
+    try:
+        os.system("hadoop fs -copyToLocal /user/usermr/output/ /home/usermr/examples/")
+    except:
+        os.system("hadoop fs -copyToLocal /home/usermr/output/ /home/usermr/examples/")
 
 else:
     
